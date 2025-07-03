6ee1fa3e9c0de9ed9d2fb07d0f108f89e593a0ca
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-02 19:47:59 -0700

    Fix Project 0: ALX-compliant JOIN queries

--- OneDrive/Documents/alx-airbnb-database/database-adv-script/joins_queries.sql
+++ OneDrive/Documents/alx-airbnb-database/database-adv-script/joins_queries.sql
@@ -1,15 +1,21 @@
 -- 1. INNER JOIN: Get all bookings and the users who made them
-SELECT bookings.id AS booking_id, users.id AS user_id, users.name
+SELECT bookings.id AS booking_id, users.id AS user_id, users.name, users.email
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
 -- 2. LEFT JOIN: Get all properties and their reviews (including properties with no reviews)
-SELECT properties.id AS property_id, reviews.id AS review_id, reviews.comment
+SELECT properties.id AS property_id, properties.name AS property_name, reviews.id AS review_id, reviews.comment
 FROM properties
 LEFT JOIN reviews ON properties.id = reviews.property_id;
 
--- 3. FULL OUTER JOIN: Get all users and bookings, even if there's no matching record
+-- 3. FULL OUTER JOIN: Get all users and bookings (include unmatched records)
 SELECT users.id AS user_id, bookings.id AS booking_id
 FROM users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT users.id AS user_id, bookings.id AS booking_id
+FROM users
+RIGHT JOIN bookings ON users.id = bookings.user_id;
 
